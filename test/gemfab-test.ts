0b8af434fa3e80fb56af317dd4308f743329cf61
nikolai <mail@nikolai.fyi>
2021-10-25 20:04:24 -0400

    extract minihat

--- test/gemfab-test.ts
+++ test/gemfab-test.ts
@@ -1,30 +1,8 @@
 const debug = require('debug')('gemfab:test')
 
-const chai = require('chai')
-chai.use(require('chai-as-promised'))
-const want = chai.expect
-
 const { ethers, artifacts, network } = require('hardhat')
 
-import * as hre from 'hardhat'
-
-const pkg = require('..')
-debug(pkg)
-
-async function send(...args) {
-  const f = args[0];
-  const fargs = args.slice(1);
-  const tx = await f(...fargs);
-  return await tx.wait()
-}
-
-async function fail(...args) {
-  const err = args[0];
-  const sargs = args.slice(1);
-  await want(send(...sargs)).rejectedWith(
-    `VM Exception while processing transaction: reverted with custom error '${err}'`
-  );
-}
+import { want, send, failRevert } from 'minihat'
 
 describe('gemfab', ()=>{
   let ali, bob, cat;
@@ -44,18 +22,13 @@ describe('gemfab', ()=>{
     gem = gem_type.attach(gemaddr);
   })
 
-  it('fixture', async () => {
-    await hre.deployments.fixture(['gemfab']);
-    debug(Object.keys(hre.deployments));
-  })
-
   it('mint ward', async () => {
     await send(gem.mint, ALI, 100);
     const bal = await gem.balanceOf(ALI);
     want(bal.toNumber()).equal(100)
 
     const gembob = gem.connect(bob);
-    await fail('ErrWard("0x40c10f19")', gembob.mint, BOB, 100);
+    await failRevert('ErrWard("0x40c10f19")', gembob.mint, BOB, 100);
   })
 
 });
