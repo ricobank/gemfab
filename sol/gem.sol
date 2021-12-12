25142609b4e83a95ca0f71748dc18d251e0c34b8
Nikolai Mushegian <924397+nmushegian@users.noreply.github.com>
2021-12-11 20:13:17 -0400

    payable entrypoints, single ward entrypoint, inline auth, cleanup etc (#44)
    * dont index name on build
    
    * payable functions
    
    * names
    
    * dont put sig in ErrAuth
    
    * payable build; formatting
    
    * consistency
    
    * dont use modifier; reorder functions
    
    * update gas costs (#46)
    
    * ErrAuth -> ErrWard
    
    * fmt no modifier, emphasize payable
    
    Co-authored-by: kbrav <3114369+kbrav@users.noreply.github.com>

--- sol/gem.sol
+++ sol/gem.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-// Copyright (C) monospace
+// Copyright (C) 2021 kevin and his friends
 // Copyright (C) 2017, 2018, 2019 dbrock, rain, mrchico
 
 // This program is free software: you can redistribute it and/or modify
@@ -29,10 +29,10 @@ contract Gem {
     mapping (address => uint)                      public nonces;
     mapping (address => bool)                      public wards;
 
-    bytes32        immutable PERMIT_TYPEHASH = keccak256(
+    bytes32 immutable PERMIT_TYPEHASH = keccak256(
         'Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)'
     );
-    bytes32        immutable DOMAIN_SEPARATOR = keccak256(abi.encode(
+    bytes32 immutable DOMAIN_SEPARATOR = keccak256(abi.encode(
         keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
         keccak256("GemPermit"),
         keccak256(bytes("0")),
@@ -42,20 +42,17 @@ contract Gem {
 
     event Approval(address indexed src, address indexed usr, uint wad);
     event Transfer(address indexed src, address indexed dst, uint wad);
-    event Ward(address indexed caller, address indexed trusts, bool bit);
+    event Ward(address indexed setter, address indexed user, bool authed);
  
     error ErrPermitDeadline();
     error ErrPermitSignature();
     error ErrOverflow();
     error ErrUnderflow();
-    error ErrAuth(bytes4 sig);
+    error ErrWard();
 
-    modifier auth() {
-        if (!wards[msg.sender]) revert ErrAuth(msg.sig);
-        _;
-    }
-
-    constructor(string memory name_, string memory symbol_) {
+    constructor(string memory name_, string memory symbol_)
+      payable
+    {
         name = name_;
         symbol = symbol_;
 
@@ -63,12 +60,51 @@ contract Gem {
         emit Ward(msg.sender, msg.sender, true);
     }
 
-    function ward(address usr, bool authed) external auth {
+    function ward(address usr, bool authed)
+      payable external
+    {
+        if (!wards[msg.sender]) revert ErrWard();
         wards[usr] = authed;
         emit Ward(msg.sender, usr, authed);
     }
 
-    function transfer(address dst, uint wad) external returns (bool) {
+    function mint(address usr, uint wad)
+      payable external
+    {
+        if (!wards[msg.sender]) revert ErrWard();
+        // only need to check totalSupply for overflow
+        unchecked {
+            uint256 prev = totalSupply;
+            if (prev + wad < prev) {
+                revert ErrOverflow();
+            }
+            balanceOf[usr] += wad;
+            totalSupply     = prev + wad;
+            emit Transfer(address(0), usr, wad);
+        }
+    }
+
+    function burn(address usr, uint wad)
+      payable external
+    {
+        if (!wards[msg.sender]) revert ErrWard();
+        // only need to check balanceOf[usr] for underflow
+        unchecked {
+            uint256 prev = balanceOf[usr];
+            uint256 next = prev - wad;
+            balanceOf[usr] = next;
+            totalSupply    -= wad;
+            emit Transfer(usr, address(0), wad);
+            if (next > prev) {
+                revert ErrUnderflow();
+            }
+        }
+    }
+
+
+    function transfer(address dst, uint wad)
+      payable external returns (bool)
+    {
         unchecked {
             uint256 prev = balanceOf[msg.sender];
             balanceOf[msg.sender] = prev - wad;
@@ -82,7 +118,7 @@ contract Gem {
     }
 
     function transferFrom(address src, address dst, uint wad)
-        external returns (bool res)
+      payable external returns (bool)
     {
         unchecked {
             uint256 prev = allowance[src][msg.sender];
@@ -103,34 +139,9 @@ contract Gem {
         }
     }
 
-    function mint(address usr, uint wad) external auth {
-        // only need to check totalSupply for overflow
-        unchecked {
-            uint256 prev = totalSupply;
-            if (prev + wad < prev) {
-                revert ErrOverflow();
-            }
-            balanceOf[usr] += wad;
-            totalSupply     = prev + wad;
-            emit Transfer(address(0), usr, wad);
-        }
-    }
-
-    function burn(address usr, uint wad) external auth {
-        // only need to check balanceOf[usr] for underflow
-        unchecked {
-            uint256 prev = balanceOf[usr];
-            uint256 next = prev - wad;
-            balanceOf[usr] = next;
-            totalSupply    -= wad;
-            emit Transfer(usr, address(0), wad);
-            if (next > prev) {
-                revert ErrUnderflow();
-            }
-        }
-    }
-
-    function approve(address usr, uint wad) external returns (bool) {
+    function approve(address usr, uint wad)
+      payable external returns (bool)
+    {
         allowance[msg.sender][usr] = wad;
         emit Approval(msg.sender, usr, wad);
         return true;
@@ -138,7 +149,8 @@ contract Gem {
 
     // EIP-2612
     function permit(address owner, address spender, uint256 value, uint256 deadline,
-                    uint8 v, bytes32 r, bytes32 s) external
+                    uint8 v, bytes32 r, bytes32 s)
+      payable external
     {
         uint nonce = nonces[owner];
         bytes32 digest = keccak256(abi.encodePacked( "\x19\x01", DOMAIN_SEPARATOR,
@@ -152,19 +164,22 @@ contract Gem {
         allowance[owner][spender] = value;
         emit Approval(owner, spender, value);
     }
+
 }
 
 contract GemFab {
     mapping(address=>bool) public built;
 
-    event Build(address indexed caller, address indexed gem, string symbol);
+    event Build(address indexed caller, address indexed gem);
 
-    function build(string memory name, string memory symbol) public returns (Gem gem) {
+    function build(string memory name, string memory symbol)
+      payable external returns (Gem gem)
+    {
         gem = new Gem(name, symbol);
         gem.ward(msg.sender, true);
         gem.ward(address(this), false);
         built[address(gem)] = true;
-        emit Build(msg.sender, address(gem), symbol);
+        emit Build(msg.sender, address(gem));
         return gem;
     }
 }
