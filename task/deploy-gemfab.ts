02e54f4e3bf98f58557f905f4e210d65701209ce
nikolai <mail@nikolai.fyi>
2021-12-19 08:56:17 -0400

    refactoring deploy task to use dpack

--- task/deploy-gemfab.ts
+++ task/deploy-gemfab.ts
@@ -1,46 +1,44 @@
-const debug = require('debug')('gemfab:task')
+const fs = require('fs')
 
 const { task } = require('hardhat/config')
+const { PackBuilder } = require('/Users/code/dpack')
 
 task('deploy-gemfab', 'deploy GemFab')
-  .addOptionalParam('outfile', 'output file to save export json')
-  .setAction(async (args, hre) => {
-    const { ethers, network } = hre
+.addParam('outfile', 'output file to save dpack (or use /dev/stdout to pipe)')
+.setAction(async (args, hre) => {
+  const { ethers, network } = hre
 
-    const [acct] = await hre.ethers.getSigners()
-    const deployer = acct.address
+  const [acct] = await hre.ethers.getSigners()
+  const deployer = acct.address
 
-    debug(`Deploying contracts using ${deployer} to ${network.name}`)
+  console.log(`Deploying contracts using ${deployer} to ${network.name}`)
 
-    const GemArtifact = await hre.artifacts.readArtifact('Gem')
-    const GemFabArtifact = await hre.artifacts.readArtifact('GemFab')
-    const GemFabDeployer = await hre.ethers.getContractFactory('GemFab')
-    const gf = await GemFabDeployer.deploy()
-    await gf.deployed()
-    debug('GemFab deployed to : ', gf.address)
+  const GemArtifact = await hre.artifacts.readArtifact('Gem')
+  const GemFabArtifact = await hre.artifacts.readArtifact('GemFab')
+  const GemFabDeployer = await hre.ethers.getContractFactory('GemFab')
+  const gf = await GemFabDeployer.deploy()
+  await gf.deployed()
 
-    const out = { types: {}, objects: {} }
-    out.types['Gem'] = {
-      typename: 'Gem',
-      artifact: GemArtifact
-    }
-    out.types['GemFab'] = {
-      typename: 'GemFab',
-      artifact: GemFabArtifact
-    }
-    out.objects['gemfab'] = {
-      name: 'gemfab',
-      typename: 'GemFab',
-      artifact: GemFabArtifact,
-      address: gf.address
-    }
-    const json = JSON.stringify(out)
+  const pb = new PackBuilder(network.name);
 
-    debug('WARN force writing file -- uprade to dpack later')
-    if (args.outfile) {
-      const fs = require('fs')
-      fs.writeFileSync(args.outfile, json)
-    } else {
-      console.log(json)
-    }
+  pb.addType({
+    typename: 'Gem',
+    artifact: GemArtifact
+  });
+  pb.addType({
+    typename: 'GemFab',
+    artifact: GemFabArtifact
   })
+
+  pb.addObject({
+    objectname: 'gemfab',
+    address: gf.address,
+    typename: 'GemFab',
+    artifact: GemFabArtifact
+  })
+
+  const pack = await pb.pack();
+  const json = JSON.stringify(pack, null, 2)
+
+  fs.writeFileSync(args.outfile, json)
+})
